2d11bacc7883dd950f4439dd40bfa8aecad1e4c7
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2021-04-24 19:46:41 +0530

    setup postgres db

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
 app = Flask(__name__)
+app.config.from_object(Config)
+DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
+engine = create_engine(DB_URI)
+
 
 @app.route("/foo")
 def hello():
@@ -7,3 +15,4 @@ def hello():
 
 if __name__ == "__main__":
   app.run()
+  
