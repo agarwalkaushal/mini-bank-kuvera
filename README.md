76e98927bc4333c4a87eb9ea30631a736b2e92ff
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2021-04-27 22:58:49 +0530

    historical statement to email

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ curl --location --request GET 'https://mini-bank-kuvera.herokuapp.com/sign_in?us
 Transact
 
 ```
-curl --location --request POST 'https://mini-bank-kuvera.herokuapp.com/transact' \
+curl --location --request POST 'https://mini-bank-kuvera.herokuapp.com/transaction/transact' \
 --header 'authtoken: 65794a30655841694f694a4b563151694c434a68624763694f694a49557a49314e694a392e65794a6c654841694f6a45324d546b314e444d344e7a6373496d6c68644349364d5459784f5451314e7a51334e79776963335669496a6f3266512e494f6b7a4e6a33424c486750575469394c326a634364644251756a45454b6c7a394a6a41747a45354f6577' \
 --header 'Content-Type: application/json' \
 --data-raw '{
@@ -29,7 +29,7 @@ curl --location --request POST 'https://mini-bank-kuvera.herokuapp.com/transact'
 Fetch Balance
 
 ```
-curl --location --request GET 'https://mini-bank-kuvera.herokuapp.com/balance' \
+curl --location --request GET 'https://mini-bank-kuvera.herokuapp.com/transaction/balance' \
 --header 'authtoken: 65794a30655841694f694a4b563151694c434a68624763694f694a49557a49314e694a392e65794a6c654841694f6a45324d546b314e444d344d544973496d6c68644349364d5459784f5451314e7a51784d69776963335669496a6f3266512e7064654e4363584c4c75696a6a356d515977435f43504d554f744c30354231386f36775730774a626b4d6b' \
 --data-raw ''
 ```
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import config
 from config import Config
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, MetaData, Table, or_
 from sqlalchemy.orm import sessionmaker
@@ -10,9 +10,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from constants import no_special_character_regex, valid_email_regex, strong_password_regex
 from utils import decode_auth_token, encode_auth_token
 import datetime
+from fpdf import FPDF
+from flask_mail import Mail, Message
 
 # Initialize app
 app = Flask(__name__)
+mail = Mail(app)
+
+# configuration of mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'samvans91@gmail.com'
+app.config['MAIL_PASSWORD'] = '****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 # Setup environment
 env_config = os.getenv("APP_SETTINGS")
@@ -82,7 +94,7 @@ def sign_in():
     return jsonify({'signed_in': False})
 
 
-@app.route('/transact', methods=["POST"])
+@app.route('/transaction/transact', methods=["POST"])
 def new_transaction():
     auth_token = request.headers['authtoken']
     user_id = decode_auth_token(auth_token)
@@ -105,11 +117,10 @@ def new_transaction():
         balance = last_transaction.balance + amount
         data = engine.execute(transactions.insert(), u_id=user_id, date_time=str(datetime.datetime.utcnow()),
                               balance=balance, amount=amount)
-
     return jsonify({'message': 'Transaction successful', 'transaction_id': data.inserted_primary_key[0]})
 
 
-@app.route('/balance', methods=["GET"])
+@app.route('/transaction/balance', methods=["GET"])
 def fetch_balance():
     auth_token = request.headers['authtoken']
     user_id = decode_auth_token(auth_token)
@@ -128,6 +139,89 @@ def fetch_balance():
         return jsonify({'balance': last_transaction.balance})
 
     return jsonify({'balance': 0})
+
+
+@app.route('/transaction/history', methods=["GET"])
+def transaction_history():
+    auth_token = request.headers['authtoken']
+    user_id = decode_auth_token(auth_token)
+
+    if user_id.isnumeric() != True:
+        return jsonify({'message': user_id})
+
+    request_data = request.get_json()
+    transactions = Table('transactions', Base.metadata,
+                         autoload=True, autoload_with=engine)
+
+    transactions_data = session.query(
+        transactions).filter_by(u_id=user_id).all()
+
+    if transactions_data == None:
+        return jsonify({'message': 'No records found'})
+
+    return jsonify({'history': transactions_data})
+
+
+@app.route('/transaction/statement', methods=["GET"])
+def transaction_statement():
+    auth_token = request.headers['authtoken']
+    user_id = decode_auth_token(auth_token)
+
+    if user_id.isnumeric() != True:
+        return jsonify({'message': user_id})
+
+    request_data = request.get_json()
+    transactions = Table('transactions', Base.metadata,
+                         autoload=True, autoload_with=engine)
+
+    transactions_data = session.query(transactions).filter_by(
+        u_id=user_id).order_by(transactions.columns.id.desc()).all()
+
+    if len(transactions_data) == 0:
+        return jsonify({'message': 'No records found'})
+
+    users = Table('users', Base.metadata, autoload=True, autoload_with=engine)
+    user = session.query(users).filter_by(id=user_id).first()
+
+    date = str(datetime.datetime.utcnow())
+    balance = str(transactions_data[0].balance)
+    email = user.email
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=15)
+    pdf.cell(200, 10, txt="Kuvera Bank", ln=1, align='C')
+    pdf.cell(200, 10, txt="Date: " + date, ln=2)
+    pdf.cell(200, 10, txt="Email: " + email, ln=3)
+    pdf.cell(200, 10, txt="Balance: " + balance, ln=4)
+    pdf.cell(200, 10, txt="S/N      " +
+             "TXN ID          " + "Amount                                    " + "Date", ln=5)
+
+    for i in range(0, len(transactions_data)):
+        ybefore = pdf.get_y()
+        column_spacing = 5
+        pdf.multi_cell(20, 10, str(i+1))
+        pdf.set_xy(20 + column_spacing + pdf.l_margin, ybefore)
+        pdf.multi_cell(20, 10, str(transactions_data[i].id))
+        pdf.set_xy(2*(20 + column_spacing) + pdf.l_margin, ybefore)
+        pdf.multi_cell(50, 10, str(transactions_data[i].amount))
+        pdf.set_xy(2*(50 + column_spacing) + pdf.l_margin, ybefore)
+        pdf.multi_cell(100, 10, str(transactions_data[i].date_time))
+
+    pdf.output(str(transactions_data[0].u_id)+"_"+date+".pdf")
+    path = str(transactions_data[0].u_id)+"_"+date+".pdf"
+    recipients = [str(email)]
+    msg = Message(
+        'Statement from Kuvera Bank',
+        sender='samvans91@gmail.com',
+        recipients=recipients
+    )
+    msg.body = 'Please find attached your historical statememt'
+    with open(os.getcwd()+"/"+path, 'rb') as fh:
+        msg.attach(filename=path, disposition="attachment",
+                   content_type="application/pdf", data=fh.read())
+    mail.send(msg)
+    return jsonify({'message': 'Please check your mail for the statement'})
 
 
 if __name__ == "__main__":
